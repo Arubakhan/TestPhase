a54feb1470007d75eae6c8aa99e39901155a05ac
Aruba <arubakhan23@gmail.com>
2019-11-16 18:09:17 +0500

    addrecord update

--- src/Pages/Addrecord/Addrecord.js
+++ src/Pages/Addrecord/Addrecord.js
@@ -1,30 +1,12 @@
 import React from 'react';
-import { AppBar, Toolbar, Typography } from "@material-ui/core";
-import Button from '@material-ui/core/Button';
-import TextField from '@material-ui/core/TextField';
-import { makeStyles } from '@material-ui/core/styles';
+import Navbar from '../../components/Navbar/Navbar';
+import { Typography } from "@material-ui/core";
+
 
 const Addrecord = () =>{
     return(
         <div>
-            <AppBar position="relative">
-        <Toolbar>
-        <Typography
-            variant="display1"
-            color="inherit"
-            style={{ marginLeft: 32, fontSize: 32, fontFamily: "italic" }}
-          >
-            Student Management System
-          </Typography>
-
-          <Button className={Addrecord.button}>Add Record</Button>       
-      <Button className={Addrecord.button}>Update Record</Button>
-          <Button className={Addrecord.button}>Delete Record</Button>
-          <Button className={Addrecord.button}>Show Record</Button>
-          <Button className={Addrecord.button}>Search Record</Button>
-          
-        </Toolbar>
-      </AppBar>
+            <Navbar />
             <Typography
             variant="display1"
             color="inherit"
@@ -32,7 +14,7 @@ const Addrecord = () =>{
           >
             Add Record
           </Typography>
-        
+
          </div>
     )
 }
