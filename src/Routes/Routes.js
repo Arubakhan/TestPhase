7fd54e6e9bd8e3190442569ad43b2b089553de9c
Aruba <arubakhan23@gmail.com>
2019-11-16 18:52:06 +0500

    shwrecord

--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -1,14 +1,14 @@
 import React, { Component } from "react";
 import { Route, Switch } from "react-router-dom";
 import Navbar from "../components/Navbar/Navbar";
-import Addrecord from "../Pages/Addrecord/Addrecord";
+import Showrecord from "../Pages/Showrecord/Showrecord";
 
 class Routes extends Component {
   render() {
     return (
       <Switch>
-<Route path="/" exact component={Addrecord} />
-<Route path="/Addrecord" exact component={Addrecord} />
+<Route path="/" exact component={Showrecord} />
+<Route path="/Showrecord" exact component={Showrecord} />
         
       </Switch>
      
